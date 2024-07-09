3d8f617e1fdd77a58466d53e492881758e530d66
JaceRogers12 <jacerogers12@gmail.com>
2024-07-09 16:19:33 -0600

    added endpoints

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,7 +10,7 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
+// import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
@@ -72,39 +72,60 @@ function GraphWrapper(props) {
                                    -- Mack 
     
     */
-
-    if (office === 'all' || !office) {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    }
+    //new
+    let citizenshipResults = [];
+    axios
+      .get("https://hrf-asylum-be-b.herokuapp.com/cases/citizenshipSummary", {
+        params: {
+          from: years[0],
+          to: years[1],
+        },
+      })
+      .then(result => {
+        citizenshipResults = result.data;
+        //new
+        //edited
+        if (office === 'all' || !office) {
+          axios
+            .get("https://hrf-asylum-be-b.herokuapp.com/cases/fiscalSummary", {
+              params: {
+                from: years[0],
+                to: years[1],
+              },
+            })
+            .then(result => {
+              let dataWithCitizenship = result.data;
+              dataWithCitizenship.citizenshipResults = citizenshipResults;
+              stateSettingCallback(view, office, [dataWithCitizenship]);
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        } else {
+          axios
+            .get("https://hrf-asylum-be-b.herokuapp.com/cases/fiscalSummary", {
+              params: {
+                from: years[0],
+                to: years[1],
+                office: office,
+              },
+            })
+            .then(result => {
+              let dataWithCitizenship = result.data;
+              dataWithCitizenship.citizenshipResults = citizenshipResults;
+              stateSettingCallback(view, office, [dataWithCitizenship]);
+            })
+            .catch(err => {
+              console.error(err);
+            });
+        }
+        //edited
+        //new
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    //new
   }
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
